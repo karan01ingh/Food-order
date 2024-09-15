8178b22091f56587763b83ead7ee6f86129e40fe
Karan01ingh <Singhkunal05046@gmail.com>
2024-09-15 23:56:23 +0530

    created with responsive design of the elements

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -6,26 +6,26 @@ import facebook from "../assets/facebook_icon.png"
 import { Link } from 'react-router-dom'
 function Footer() {
   return (
-    <div className='w-screen h-auto  bg-zinc-800 pl-1 pt-1 pr-1 flex flex-col md:pl-14 md:pt-14 md:pr-14 '>
-        <div className='h-auto w-full flex flex-row border-b-2 border-slate-200 justify-between pb-10'>
+    <div className='w-screen h-auto  bg-zinc-800 pl-4 pt-4 pr-1 flex flex-col md:pl-14 md:pt-14 md:pr-14 '>
+        <div className='h-auto w-full flex xs:flex-row justify-center items-center flex-col border-b-2 border-slate-200  pb-10 '>
             <div className='w-1/2'>
                 <img src={logo} alt=""/>
                 <br />
-                <h1 className='text-slate-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eveniet quis, iure repellat reprehenderit quasi maxime quam, saepe commodi minus est placeat accusamus suscipit sapiente. Maxime temporibus molesti</h1>
+                <h1 className='text-slate-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eveniet quis, iure repellat reprehenderit quasi maxime quam, saepe commodi minus est placeat accusamus suscipit sapiente. Maxime temporibus molesti Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores harum neque vel itaque, cupiditate omnis repudiandae sed dolorem quod perferendis animi, beatae nostrum sit! Hic inventore veritatis explicabo vitae doloribus.</h1>
                 <br />
-                <div className='flex flex-row jstify sm:p-2 '>
+                <div className=' w-full xs:w-52 h-auto flex flex-row justify-between sm:p-2  '>
                     <Link to={"https://x.com/"}><img src={twitter} alt="" className='m-1' /></Link><Link to={"https://www.linkedin.com/"}><img src={linkedin} alt="" className='m-1'/></Link><Link to={"https://www.facebook.com/"}><img src={facebook} alt="" className='m-1'/></Link>
                 </div>
             </div>
             <div className='w-1/2 flex flex-col md:flex-row justify-around md:justify-around '>
-            <div className='md:w-1/2 w-full h-auto md:pt-0 text-center  pl-1'>
+            <div className='md:w-1/2 w-full h-auto md:pt-0 text-center  pl-1 mt-5 xs:mt-0'>
                 <h1 className='text-slate-200  font-semibold md:text-3xl text-lg'>COMPANY</h1>
                 <Link to={"/"}><h1 className='text-slate-200 font-semibold text-sm md:text-lg pt-2'>Home</h1></Link>
                 <Link><h1 className='text-slate-200 font-semibold text-sm md:text-lg pt-2 '>About Us</h1></Link>
                 <Link><h1 className='text-slate-200 font-semibold text-sm md:text-lg pt-2 '> Delievery</h1></Link>
                 <Link><h1 className='text-slate-200 font-semibold text-sm md:text-lg pt-2 '>Privacy Policy</h1></Link>
             </div>
-            <div className='md:w-1/2 w-full h-auto md:pt-0 text-center '>
+            <div className='md:w-1/2 w-full h-auto md:pt-0 text-center xs:mt-5 md:mt-0'>
                 <h1 className='text-slate-200 md:text-3xl font-semibold text-lg'>GET IN TOUCH</h1>
                 <h1 className='text-slate-200 font-semibold text-sm md:text-lg pt-2'>+1-212-456-7890</h1>
                 <h1 className='text-slate-200 font-semibold pt-2'>conatct@tomato.com</h1>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+       'xs':'450px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       animation: {
         fadeIn: 'fadeIn 1s ease-out', // You can adjust the duration and easing as needed
